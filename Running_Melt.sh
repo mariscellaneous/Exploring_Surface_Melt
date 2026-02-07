341611cd841fdfdf808b201802458e1f6d52cc62
Marissa Dattler <marissadattler@gmail.com>
2026-02-07 18:23:22 -0500

    Add files via upload

---
+++ Running_Melt.sh
@@ -0,0 +1,49 @@
+bm=1
+by=2014
+bd=1
+
+em=12
+ey=2018
+ed=31 
+
+
+# python runningAWSfor18H_update2.py 0 2014 1 1 2019 5 31
+# python runningAWSfor18H_update2.py 1 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 2 $by $bm $bd $ey $em $ed
+python runningAWSfor18H_update_3rd.py 3 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 4 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 5 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 6 2012 7 2 2019 5 31 
+# python runningAWSfor18H_update2.py 7 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 8 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 9 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 10 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 11 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 12 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 13 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 14 $by $bm $bd $ey $em $ed
+
+# For final run 
+# bm=1
+# by=2014
+# bd=1
+
+# em=12
+# ey=2018
+# ed=31
+
+#run final
+
+# python runningAWSfor18H_update2.py 0 $by $bm $bd 2019 5 31
+# python runningAWSfor18H_update2.py 1 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 2 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 3 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 4 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 5 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 6 2012 7 2 $ey $em $ed
+# python runningAWSfor18H_update2.py 7 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 8 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 9 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 10 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 11 $by $bm $bd $ey $em $ed
+# python runningAWSfor18H_update2.py 12 $by $bm $bd $ey $em $ed
